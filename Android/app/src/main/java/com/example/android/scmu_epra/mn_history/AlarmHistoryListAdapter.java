4230bea85256417fcd57658ff0ca15320602d587
filipealmeida <filipewooten20@gmail.com>
2018-06-16 13:18:39 +0100

    History search implemented

--- Android/app/src/main/java/com/example/android/scmu_epra/mn_history/AlarmHistoryListAdapter.java
+++ Android/app/src/main/java/com/example/android/scmu_epra/mn_history/AlarmHistoryListAdapter.java
@@ -2,11 +2,14 @@ package com.example.android.scmu_epra.mn_history;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,25 +18,46 @@ import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.example.android.scmu_epra.R;
 import com.example.android.scmu_epra.connection.Row;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AlarmHistoryListAdapter extends ArrayAdapter<Row> {
+public class AlarmHistoryListAdapter extends ArrayAdapter<Row> implements Filterable {
 
     private Context context;
-    private List<Row> list;
+    private List<Row> listOriginal;
+    private List<Row> listRow;
     private TextDrawable.IBuilder builder;
     private ColorGenerator generator;
+    private DescriptionFilter descriptionFilter;
+
 
     public AlarmHistoryListAdapter(Context context, int resource, List<Row> items) {
         super(context, 0, items);
         this.context = context;
-        this.list = items;
+        this.listRow = items;
+        this.listOriginal = items;
         this.builder = TextDrawable.builder()
                 .beginConfig()
                 .withBorder(4)
                 .endConfig()
                 .round();
         this.generator = ColorGenerator.MATERIAL;
+    }
+
+    @Override
+    public int getCount() {
+        return listRow.size();
+    }
+
+    @Nullable
+    @Override
+    public Row getItem(int position) {
+        return listRow.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
@@ -44,7 +68,7 @@ public class AlarmHistoryListAdapter extends ArrayAdapter<Row> {
             v = LayoutInflater.from(this.context).inflate(R.layout.frag_history_list_item, parent, false);
         }
 
-        Row item = this.list.get(position);
+        Row item = this.listRow.get(position);
         String evento = item.getEvento();
         String date = item.getData();
 
@@ -66,6 +90,55 @@ public class AlarmHistoryListAdapter extends ArrayAdapter<Row> {
 
         return v;
     }
+
+
+    @Override
+    public Filter getFilter() {
+        if (descriptionFilter == null) {
+            descriptionFilter = new DescriptionFilter();
+        }
+        return descriptionFilter;
+    }
+
+
+    private class DescriptionFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint != null && constraint.length() > 0) {
+                ArrayList<Row> filterList = new ArrayList<Row>();
+                for (int i = 0; i < listOriginal.size(); i++) {
+                    if ((listOriginal.get(i).getEvento().toUpperCase())
+                            .contains(constraint.toString().toUpperCase())) {
+
+                        Row row = new Row(
+                                listOriginal.get(i).getId(),
+                                listOriginal.get(i).getEvento(),
+                                listOriginal.get(i).getData());
+
+                        filterList.add(row);
+                    }
+                }
+                results.count = filterList.size();
+                results.values = filterList;
+            } else {
+                results.count = listOriginal.size();
+                results.values = listOriginal;
+            }
+            return results;
+
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint,
+                                      FilterResults results) {
+            listRow = (List<Row>) results.values;
+            notifyDataSetChanged();
+        }
+
+    }
+
 }
 
 
--- Android/app/src/main/java/com/example/android/scmu_epra/mn_home/HomeListAdapter.java
+++ Android/app/src/main/java/com/example/android/scmu_epra/mn_home/HomeListAdapter.java
@@ -66,6 +66,7 @@ public class HomeListAdapter extends ArrayAdapter<AreaItem> {
         String json = sharedPref.getString(Constants.SIGNED_ACCOUNT_TAG, "");
         UserItem currentAccount = gson.fromJson(json, UserItem.class);
 
+        //TODO: corrigir problema: está a mudar o switch qdo nao devia. Desactivar o ontouch ou algo do género
         switch1.setEnabled(currentAccount != null && currentAccount.getPermissions().contains(item.getId()));
 
         return v;
