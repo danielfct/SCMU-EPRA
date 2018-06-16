08c8808774aceb546b2cc436c70ba2cae448e71a
Luis Martins <luis_5566@hotmail.com>
2018-06-16 20:52:13 +0100

    BurglaryManagement - phone and turn off alarm

--- Android/app/src/main/java/com/example/android/scmu_epra/mn_burglaryManag/BurglaryManagementFragment.java
+++ Android/app/src/main/java/com/example/android/scmu_epra/mn_burglaryManag/BurglaryManagementFragment.java
@@ -1,11 +1,17 @@
 package com.example.android.scmu_epra.mn_burglaryManag;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.AppCompatButton;
@@ -28,6 +34,7 @@ import com.example.android.scmu_epra.connection.DownloadStatus;
 import com.example.android.scmu_epra.connection.GetBurglaryHistoryJsonData;
 import com.example.android.scmu_epra.connection.GetHistoryJsonData;
 import com.example.android.scmu_epra.connection.GetJsonData;
+import com.example.android.scmu_epra.connection.PostJsonData;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,9 +45,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BurglaryManagementFragment extends Fragment
-        implements GetBurglaryHistoryJsonData.OnDataAvailable, GetJsonData.OnDataAvailable {
+        implements GetBurglaryHistoryJsonData.OnDataAvailable, GetJsonData.OnDataAvailable, PostJsonData.OnStatusAvailable {
 
     public static final String TAG = "BurglaryManagement";
+    private static final int REQUEST_LOCATION = 1;
 
     @BindView(R.id.bottom_sheet)
     LinearLayout bottomListView;
@@ -99,10 +107,34 @@ public class BurglaryManagementFragment extends Fragment
 
         mHandler.postDelayed(mRunnable, Constants.DATA_UPDATE_FREQUENCY);
 
-        notifyPoliceButton.setOnClickListener(v -> {});
-        simulateButton.setOnClickListener(v -> {});
-        turnOffAlarmButton.setOnClickListener(v -> {});
         ignoreButton.setOnClickListener(v -> {});
+        simulateButton.setOnClickListener(v -> {});
+        turnOffAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executePostJson("https://test966996.000webhostapp.com/api/post_alarminfo.php", "estadoAtual=0");
+            }
+        });
+        notifyPoliceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:960000000"));
+
+                if (ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onClick: phone call - permission not granted!");
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            REQUEST_LOCATION);
+                    return;
+                }
+
+                Log.d(TAG, "onClick: PERMISSION GRANTED!!");
+                startActivity(callIntent);
+            }
+        });
 
         BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomListView);
         baseLayout.addOnLayoutChangeListener((v, i, i1, i2, i3, i4, i5, i6, i7) -> {
@@ -152,18 +184,62 @@ public class BurglaryManagementFragment extends Fragment
     @Override
     public void onDataAvailable(JSONObject data) {
         try {
-            String areaAtual = data.getString("atual");
-            String areaEntrada = data.getString("entrada");
-            String pessoasNotificadas = data.getString("pessoasNotificadas");
-            currentRoomText.setText(areaAtual);
-            entryPointText.setText(areaEntrada);
-            usersNotifiedText.setText(pessoasNotificadas);
+            if (data != null) {
+                String areaAtual = data.getString("atual");
+                String areaEntrada = data.getString("entrada");
+                String pessoasNotificadas = data.getString("pessoasNotificadas");
+                currentRoomText.setText(areaAtual);
+                entryPointText.setText(areaEntrada);
+                usersNotifiedText.setText(pessoasNotificadas);
+            }
 
             GetJsonData getJsonData = new GetJsonData(this, "https://test966996.000webhostapp.com/api/get_tracking.php?id=1");
             getJsonData.execute();
 
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+
+    private final void executePostJson(String url, String... params) {
+        PostJsonData postJsonData = new PostJsonData(this, url);
+        postJsonData.execute(params);
+    }
+
+    @Override
+    public void onStatusAvailable(Boolean status) {
+        if (status) {
+            Snackbar.make(getView(), "Alarm turned off.", Snackbar.LENGTH_SHORT).show();
+        } else {
+            Snackbar.make(getView(), "Unable to connect to the server.", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:960000000"));
+                    startActivity(callIntent);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 
