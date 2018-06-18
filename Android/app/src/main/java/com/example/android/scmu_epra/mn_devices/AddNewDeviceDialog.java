12d4db6df8dab610d00e07c8a8ccca396c4c61f9
filipealmeida <filipewooten20@gmail.com>
2018-06-18 16:03:58 +0100

    cant insert device without name

--- Android/app/src/main/java/com/example/android/scmu_epra/mn_devices/AddNewDeviceDialog.java
+++ Android/app/src/main/java/com/example/android/scmu_epra/mn_devices/AddNewDeviceDialog.java
@@ -3,6 +3,7 @@ package com.example.android.scmu_epra.mn_devices;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
@@ -10,10 +11,12 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.SwitchCompat;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.android.scmu_epra.Constants;
@@ -31,6 +34,7 @@ public class AddNewDeviceDialog extends DialogFragment {
     TextInputLayout deviceNameLayout;
     TextInputEditText inputDeviceName;
     Spinner deviceType;
+    Spinner deviceArea;
     SwitchCompat deviceStatus;
 
     public AddNewDeviceDialog() {
@@ -47,27 +51,47 @@ public class AddNewDeviceDialog extends DialogFragment {
         deviceNameLayout = v.findViewById(R.id.device_name_layout);
         inputDeviceName= v.findViewById(R.id.input_device_name);
         deviceType = v.findViewById(R.id.device_type);
+        deviceArea = v.findViewById(R.id.device_area);
         deviceStatus = v.findViewById(R.id.device_status);
 
         String[] items = new String[]{"Sensor", "Actuator", "Simulator"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
         deviceType.setAdapter(adapter);
 
+//        String[] areas = AISHFhilALDFHASLDFh
+//        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+//        deviceType.setAdapter(adapter2);
+
         b.setView(v);
 
         b.setTitle("New Device");
-        b.setPositiveButton("Save", (dialog, whichButton) -> newDevice());
+        b.setPositiveButton("Save", null);
         b.setNegativeButton("Cancel", (dialog, whichButton) -> dialog.dismiss());
 
-        return b.create();
-    }
+        Dialog dialog = b.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener((view) -> {
 
-    private void newDevice() {
-        PostJsonData postJsonData = new PostJsonData((MainActivity) getActivity(),
-                "https://test966996.000webhostapp.com/api/post_devices.php", Constants.Status.ADD_NEW_DEVICE);
-        postJsonData.execute("nome=" + inputDeviceName.getText().toString(),
-                "tipo=" + deviceType.getSelectedItem().toString(),
-                "ligado=" + (deviceStatus.isChecked() ? "1" : "0"));
+                        String name = inputDeviceName.getText().toString();
+                        if (name.equals("")) {
+                            deviceNameLayout.setError("The name can't be empy.");
+                            return;
+                        }
+                        else {
+                            PostJsonData postJsonData = new PostJsonData((MainActivity) getActivity(),
+                                    "https://test966996.000webhostapp.com/api/post_devices.php", Constants.Status.ADD_NEW_DEVICE);
+                            postJsonData.execute("nome=" + name,
+                                    "tipo=" + deviceType.getSelectedItem().toString(),
+                                    "ligado=" + (deviceStatus.isChecked() ? "1" : "0"));
+                            dialog.dismiss();
+                        }
+                    });
+            }
+        });
+        return dialog;
     }
 
 }
